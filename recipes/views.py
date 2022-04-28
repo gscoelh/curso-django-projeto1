f71831327805a41e45da4b3bde1562d8d0d05c70
gscoelh <geraldocoelho1957@gmail.com>
2022-04-27 23:02:52 -0300

    criação do primeiro APP

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):  
+    return HttpResponse('home')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+        
+def contato(request):
+    return HttpResponse('contato')
