340c02784fbd8517cd77667b460acae351a00f58
gscoelh <geraldocoelho1957@gmail.com>
2022-05-01 22:05:00 -0300

    AJUSTES DO PRIMEIRO CABEÇALHO COM HTML

--- recipes/views.py
+++ recipes/views.py
@@ -4,14 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):  
-    return render(request, 'recipes/home.html',context={
-        'name':'mostra como passa por parametro no render'
+    return render(request, 'recipes/pages/home.html' ,context={
+        'name' :'mostra como passa por parametro no render'
     })
 
 
-def sobre(request):
-    return HttpResponse('sobre')
-
-        
-def contato(request):
-    return render(request ,'temp/temp.html')
